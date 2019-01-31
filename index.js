beaa0f25374bc4158fdb8f0d6ceee8d571c20b9e
carl0sarb0leda <carlosaepn@gmail.com>
2019-01-31 23:44:48 +1300

    basic config complete

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react'; //divide in components or react elements
+import ReactDOM from 'react-dom'; //this is for render the components
+
+//console.log('Hiii ;)');
+const app = document.getElementById('app')
+const hi = <h1>Helloooo</h1>;
+
+//ReactDOM.render(what i am going to render, where i am going to render)
+ReactDOM.render(hi,app);
