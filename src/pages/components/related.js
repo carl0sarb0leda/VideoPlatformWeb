31d12ffdf96a5f7070a862ad114bfe359c45c429
carl0sarb0leda <carlosaepn@gmail.com>
2019-11-10 03:24:29 +1300

    working on navigation bar

--- src/pages/components/related.js
+++ src/pages/components/related.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import logo from '../../../images/logo.png'
 import './related.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faLightbulb, faCompactDisc, faHeadphones } from '@fortawesome/free-solid-svg-icons'
 
 function Related (){
     return(
-        <div className="Related">
+        <div className="Background">
+            <div className="Related">
             <img src={logo} width={250}></img>
             <h2>Video Platform Demo</h2>
-            <p>Interesting</p>
-            <i class="fas fa-theater-masks"></i>
-            <p>Coding</p>
-            <p>Pop</p>
-            <p>Relax</p>
+            <p><FontAwesomeIcon icon={faLightbulb}/> Interesting</p>
+            <p><FontAwesomeIcon icon={faCoffee}/> Coding</p>
+            <p><FontAwesomeIcon icon={faCompactDisc} pulse/> Pop</p>
+            <p><FontAwesomeIcon icon={faHeadphones}/> Relax</p>
+            </div>
         </div>
     )
 }
